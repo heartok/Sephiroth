ef9e6bc3ffead0a86c8d24297448826c46ac3f98
Michael Bartron <mikebartron@gmail.com>
2016-06-14 23:03:03 -0400

    Add feature for player to ignore resting.

--- Sephiroth.Tests/UnitTest1.cs
+++ Sephiroth.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
-﻿using Ploeh.AutoFixture;
+﻿using System;
+using System.Collections.Generic;
+using Ploeh.AutoFixture;
 using Xunit;
 
 namespace Sephiroth.Tests
@@ -10,11 +12,7 @@ namespace Sephiroth.Tests
         {
             // Fixture setup
             var fixture = new Fixture();
-            var sut = fixture.Build<Player>()
-                .With(p => p.LowHealth, 25)
-                .With(p => p.HighHealth, 75)
-                .With(p => p.Health, 50)
-                .Create();
+            var sut = fixture.Customize(new InjuredPlayerCustomization()).Create<Player>();
             // Exercise system
             var result = sut.MakeDecision();
             // Verify outcome
@@ -27,16 +25,50 @@ namespace Sephiroth.Tests
         {
             // Fixture setup
             var fixture = new Fixture();
-            var sut = fixture.Build<Player>()
-                .With(p => p.LowHealth, 25)
-                .With(p => p.HighHealth, 75)
-                .With(p => p.Health, 100)
-                .Create();
+            var sut = fixture.Customize(new HealthyPlayerCustomization()).Create<Player>();
             // Exercise system
             var result = sut.MakeDecision();
             // Verify outcome
             Assert.IsType<NoDecision>(result);
             // Teardown
+        }
+
+        [Fact]
+        public void GivenAPlayer_WhenPlayerIsInjuredWithNoRestDecision_ReturnsNoDecision()
+        {
+            // Fixture setup
+            var fixture = new Fixture();
+            var sut = fixture.Customize(new InjuredPlayerCustomization()).Create<Player>();
+            sut.Decisions.Clear();
+            // Exercise system
+            var result = sut.MakeDecision();
+            // Verify outcome
+            Assert.IsType<NoDecision>(result);
+            // Teardown
+        }
+    }
+
+    public class HealthyPlayerCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Customize<Player>(composer => composer
+                .Without(p => p.Decisions)
+                .With(p => p.LowHealth, 25)
+                .With(p => p.HighHealth, 75)
+                .With(p => p.Health, 100));
+        }
+    }
+
+    public class InjuredPlayerCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Customize<Player>(composer => composer
+                .Without(p => p.Decisions)
+                .With(p => p.LowHealth, 25)
+                .With(p => p.HighHealth, 75)
+                .With(p => p.Health, 50));
         }
     }
 
@@ -53,13 +85,27 @@ namespace Sephiroth.Tests
         public int LowHealth { get; set; }
         public int HighHealth { get; set; }
         public int Health { get; set; }
+        public HashSet<Type> Decisions { get; set; }
+
+        public Player()
+        {
+            Decisions = new HashSet<Type> {typeof(RestDecision)};
+        }
 
         public IDecision MakeDecision()
         {
-            if(LowHealth < Health && HighHealth > Health) return new RestDecision();
+            if (LowHealth < Health && HighHealth > Health)
+            {
+                if (Decisions.Contains(typeof(RestDecision)))
+                {
+                    return new RestDecision();
+                }                
+            }
             return new NoDecision();
         }
     }
 
-    public interface IDecision { }
+    public interface IDecision
+    {
+    }
 }
